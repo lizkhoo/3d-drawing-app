ff98ba3b34036abd02e81c39f04c67eed8b81cd9
Liz Khoo <emkhoo@gmail.com>
2013-11-19 19:16:46 -0500

    drawing app not working

--- app.py
+++ app.py
@@ -1,19 +1,22 @@
 import os, datetime
 import re
-from flask import Flask, request, render_template, redirect, abort
+from flask import Flask, request, render_template, redirect, abort, flash, json, jsonify
 from unidecode import unidecode
 
 # mongoengine database module
 from flask.ext.mongoengine import MongoEngine
 
 app = Flask(__name__) #create our flask app
-app.config['CSRF_ENABLED'] = False
+app.config['CSRF_ENABLED'] = True
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
 
 #-- Database Connection --
 # MongoDB connection to MongoLab's database
 app.config['MONGODB_SETTINGS'] = {'HOST':os.environ.get('MONGOLAB_URI'), 'DB': 'mofongousers'}
 app.logger.debug("Connecting to MongoLabs")
 db = MongoEngine(app)
+
+# app.run(debug = True)
 
 # import data models
 import models
@@ -26,7 +29,23 @@ categories = ['home good','accessory','art piece']
 @app.route("/", methods=['GET','POST'])
 def index():
 
-	if request.method == "POST":
+	# render template
+	templateData = {
+		'designs' : models.Design.objects(),
+		'categories' : categories
+	}
+
+	return render_template('main-gallery.html', **templateData)
+
+
+@app.route("/draw", methods=['GET','POST'])
+def draw():
+
+	design = models.Design.objects()
+	design_form = models.DesignForm(request.form)
+	newdesign = ""
+
+	if request.method == "POST" and design_form.validate():
 
 		# get form data
 		design = models.Design()
@@ -34,26 +53,38 @@ def index():
 		design.title = request.form.get('title', 'no title')
 		design.slug = slugify(design.title + " " + design.designer)
 		design.design = request.form.get('design', '')
-		#design.categories = request.form.get('categories')
+		design.categories = request.form.getlist('categories')
+		# use .getlist for HTTP Get forms with checkboxes, bc it returns a list
 
 		design.save()
 
-		# direct browser to newly created design detail page
-		return redirect('/designs/%s' % design.slug)
+		# direct browser to homepage
+		return redirect('/')
 
 	else:
-		# form management stuff
+	
+		#form management stuff
 		if request.method == "POST" and request.form.getlist('categories'):
 			for c in request.form.getlist('categories'):
-				idea_form.categories.append_entry(c)
+				design_form.categories.append_entry(c)
 
-		# render template
+		if request.method == "POST" and request.form.get('design','') is not '':
+			newdesign = request.form.get('design')
+
 		templateData = {
-			'designs' : models.Design.objects(),
-			'categories' : categories
+			'categories' : categories,
+			'form' : design_form,
+			'newdesign' : newdesign,
+			'design' : design
 		}
 
-		return render_template("main.html", **templateData)
+		return render_template('david-drawing.html', **templateData)
+
+
+@app.route("/about")
+def about():
+
+	return "this is the about page"
 
 
 @app.route("/categories/<cat_name>")
@@ -80,21 +111,142 @@ def by_category(cat_name):
 	return render_template('category_listing.html', **templateData)
 
 
-@app.route("/designs/<proj_slug>")
+@app.route("/designs/<proj_slug>", methods=['GET', 'POST'])
 def design_detail(proj_slug):
 
-	# pull all metadata about a design by the proj_slug
+	# update entry with new data
+	if request.method == 'POST':
+
+		try:
+			design = models.Design.objects.get(slug=proj_slug)
+			
+		except:
+			raise
+
+		designForm = models.DesignForm(request.form)
+
+		if designForm.validate():
+			updateData = {
+				'set__title' : request.form.get('title'),
+				'set__designer' : request.form.get('designer'),
+				'set__design' : request.form.get('design'),
+				'set__categories' : request.form.getlist('categories')
+			}
+			design.update(**updateData)
+
+			flash('Update successful!')
+
+			templateData = {
+				'design' : models.Design.objects(),
+				'categories' : categories
+			}
+
+			return redirect('/')
+
+		else: 
+			
+			templateData = {
+				'design' : design,
+				'design_id' : design.id,
+				'form' : designForm,
+				'categories' : categories
+			}
+
+			return render_template('drawing-view.html', **templateData)
+	
+	# just show the design entry
+	else:
+		# get the design convert it to the model form, this prepopulates the form
+		try:
+
+			designDocument = models.Design.objects.get(slug=proj_slug) # query for single document in Mongo where slug = proj_slug
+			designForm = models.DesignForm(obj=designDocument) # populate the models wtform with the Mongo document (optional)
+
+		except:
+			raise
+
+		# get all the data to populate template
+		templateData = {
+			'design' : designDocument,
+			'design_id' : designDocument.id,
+			'form' : designForm,
+			'categories' : categories
+		}
+
+		return render_template('drawing-view.html', **templateData)
+
+
+
+@app.route('/data/designs')
+def data_designs():
+
+	designs = models.Design.objects().order_by('+timestamp').limit(10)
+
+	if designs:
+
+		#list to hold array file
+		alldesigns = []
+
+		#prep data for json
+		for i in designs:
+			tmpData = dataToDict(i)
+
+			# insert dictionary into list
+			alldesigns.append( tmpData )
+
+		#prep dictionary for JSON return
+		data = {
+			'status' : 'OK',
+			'designs' : alldesigns
+		}
+
+		#jasonify from Flask converts dictionary and sets mime type to 'application/json'!
+		return jsonify(data)
+
+	else:
+		error = {
+			'status' : 'error',
+			'message' : 'unable to retrieve data!'
+		}
+		return jsonify(error)
+
+
+@app.route('/data/designs/<id>')
+def data_design(id):
+
+# query for designs - return oldest first, limit 10
+
 	try:
-		design = models.Design.objects.get(slug=proj_slug)
+		design = models.Design.objects.get(id=id)
+		if design:
+			tmpData = dataToDict(design)
+			data = {
+				'status' : 'OK',
+				'design' : tmpData
+			}
+
+		return jsonify(data)
 
 	except:
-		abort(404)
+		error = {
+			'status' : 'error',
+			'message' : 'unable to retrieve data!' 
+		}
 
-	templateData = {
-		'design' : design
+		return jsonify(error)
+
+
+def dataToDict(design):
+	# make dictionary
+	tmpData = {
+		'id' : str(design.id),
+		'designer' : design.designer,
+		'title' : design.title,
+		'design' : design.design,
+		'timestamp' : str(design.timestamp)
 	}
 
-	return render_template('design_detail.html', **templateData)
+	return tmpData
 
 
 @app.errorhandler(404)
--- models.py
+++ models.py
@@ -1,18 +1,24 @@
 # mongoengine database module
 from mongoengine import *
+from flask.ext.mongoengine.wtf import model_form
+
 from datetime import datetime
 import logging
 
 
 class Design(Document):
 	
-	designer = StringField(max_length=120, required=True, verbose_name="Your Name")
-	title = StringField(max_length=120, required=True)
+	designer = StringField(max_length=120, required=True, verbose_name="Name")
+	title = StringField(max_length=120, required=True, verbose_name="Drawing Title")
 	slug = StringField()
-	design = StringField(required=True, verbose_name="Title of Design")
+	design = StringField(verbose_name="Design Data")
+	image = StringField(required=True, max_length=255)
 
 	# Timestamp will record the date and time idea was created.
 	timestamp = DateTimeField(default=datetime.now())
 
-	# Category is a list of STrings
+	# Category is a list of Strings
 	categories = ListField(StringField(max_length=30))
+
+DesignForm = model_form(Design)
+
